ee4fe74a2207ea5fc4245ef604020e8aaacaf90e
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2022-12-02 22:26:14 +0100

    Day 1: Part 2

--- day01/day01.py
+++ day01/day01.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 
-with open('input.txt', 'r') as f:
-    data = f.read().split('\n')
 
-max_calories = 0
-current_calories = 0
+def part_1():
+    data = parse_input()
+    max_calories = 0
+    current_calories = 0
 
-for row in data:
-    if not row:
-        max_calories = max(max_calories, current_calories)
-        current_calories = 0
-        continue
+    for row in data:
+        if not row:
+            max_calories = max(max_calories, current_calories)
+            current_calories = 0
+            continue
 
-    current_calories += int(row)
+        current_calories += int(row)
 
-print(max_calories)
+    return max_calories
+
+
+def part_2():
+    data = parse_input()
+    inventory = []
+    current_calories = 0
+
+    for row in data:
+        if not row:
+            inventory.append(current_calories)
+            current_calories = 0
+            continue
+
+        current_calories += int(row)
+
+    inventory.sort(reverse=True)
+    return sum(inventory[:3])
+
+
+def parse_input():
+    with open('input.txt', 'r') as f:
+        data = f.read().split('\n')
+    return data
+
+
+if __name__ == '__main__':
+    print(f'Part 1: {part_1()}')
+    print(f'Part 2: {part_2()}')
