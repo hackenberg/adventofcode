ac1c40b1bf721b31606c34da5906e096e42941c1
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2022-12-03 17:50:41 +0100

    Day 2: Part 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
-
-ROCK = ['A', 'X']
-PAPER = ['B', 'Y']
-SCISSORS = ['C', 'Z']
+from abc import ABC
 
 
-class Choice:
+class Choice(ABC):
+    """A Choice is either Rock, Paper or Scissors."""
     def __init__(self, beats, draws, loses):
         self.beats = beats
         self.draws = draws
         self.loses = loses
 
     def versus(self, opponent):
+        """Solution for Part 1"""
+        assert opponent in ['A', 'B', 'C']
         if opponent in self.beats:
             return 6
         if opponent in self.draws:
             return 3
         if opponent in self.loses:
             return 0
+
+    def fulfill_prediction(self, prediction):
+        """Solution for Part 2"""
+        assert prediction in ['X', 'Y', 'Z']
+        if prediction == 'X':
+            return CHOICES[self.beats[0]].value + 0
+        if prediction == 'Y':
+            return CHOICES[self.draws[0]].value + 3
+        if prediction == 'Z':
+            return CHOICES[self.loses[0]].value + 6
 
 
 class Rock(Choice):
@@ -38,6 +48,10 @@ class Scissors(Choice):
         super().__init__(PAPER, SCISSORS, ROCK)
 
 
+ROCK = ['A', 'X']
+PAPER = ['B', 'Y']
+SCISSORS = ['C', 'Z']
+
 CHOICES = {
     'A': Rock(), 'X': Rock(),
     'B': Paper(), 'Y': Paper(),
@@ -57,8 +71,11 @@ def part_1():
 
 def part_2():
     data = parse_input()
-    # TODO
-    return None
+    score = 0
+    for row in data:
+        opponent = CHOICES[row[0]]
+        score += opponent.fulfill_prediction(row[1])
+    return score
 
 
 def parse_input():
