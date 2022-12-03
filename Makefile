c6a42d3a14ca07b9a957e2644e73a7e1fcec8110
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2022-12-03 18:59:31 +0100

    Update template and Makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 day%:
 	mkdir $@
 	cp -n template.py $@/$@.py
+	chmod 755 $@/$@.py
--- template.py
+++ template.py
@@ -2,21 +2,20 @@
 
 
 def part_1():
-    data = parse_input()
-    return None
+    pass
 
 
 def part_2():
-    data = parse_input()
-    return None
+    pass
 
 
 def parse_input():
     with open('input.txt', 'r') as f:
-        data = f.read().split('\n')
+        data = f.read().split('\n')[:-1]
     return data
 
 
 if __name__ == '__main__':
-    print(f'Part 1: {part_1()}')
-    print(f'Part 2: {part_2()}')
+    data = parse_input()
+    print(f'Part 1: {part_1(data)}')
+    print(f'Part 2: {part_2(data)}')
