fea441502ea7f884f980e13bab6a9bd3afe13f8a
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2022-12-05 12:40:27 +0100

    Day 5: Part 1

---
+++ day05/day05.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+
+def part_1(data):
+    stacks, instructions = data
+    for times, src, dest in instructions:
+        for i in range(0, times):
+            crate = stacks[src].pop()
+            stacks[dest].append(crate)
+    return get_message(stacks)
+
+
+def get_message(stacks):
+    return ''.join([s[-1] for s in stacks])
+
+
+def part_2(data):
+    # TODO
+    pass
+
+
+def parse_input():
+    with open('input.txt', 'r') as f:
+        data = f.read()
+    drawing, instructions = data.rstrip().split('\n\n')
+    return parse_drawing(drawing), parse_instructions(instructions)
+
+
+def parse_drawing(drawing):
+    # Split into rows and remove indices
+    rows = drawing.split('\n')[:-1]
+    stacks = []
+    for column in range(1, len(rows[0]), 4):
+        # Parse column and remove whitespace
+        crates = ''.join([row[column] for row in rows]).lstrip()
+        stacks.append(list(crates[::-1]))
+    return stacks
+
+
+def parse_instructions(instructions):
+    instructions = instructions.rstrip().split('\n')
+    def parse_line(instr):
+        instr = instr.replace('move ', '')
+        instr = instr.replace('from ', '')
+        instr = instr.replace('to ', '')
+        instr = [int(i) for i in instr.split()]
+        # Make life easier by using zero-based indexing
+        instr[1] -= 1
+        instr[2] -= 1
+        return instr
+    return [parse_line(line) for line in instructions]
+
+
+if __name__ == '__main__':
+    data = parse_input()
+    print(f'Part 1: {part_1(data)}')
+    print(f'Part 2: {part_2(data)}')
