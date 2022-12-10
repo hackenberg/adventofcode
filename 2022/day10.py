54aff2a97b2444088b3a5ee5fe57234e91aaab83
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2022-12-10 15:28:48 +0100

    Day 10: Part 1

---
+++ 2022/day10.py
@@ -0,0 +1,25 @@
+#import IPython; IPython.embed(colors="neutral")
+#import ipdb; ipdb.set_trace()
+
+
+def p1(f):
+    instructions = f.read().splitlines()
+    args = [1] + [0] * (len(instructions) * 2)
+    cycle = 0
+
+    for instr in instructions:
+        match instr.split():
+            case ["addx", arg]:
+                cycle += 2
+                args[cycle] = int(arg)
+            case ["noop"]:
+                cycle += 1
+            case other:
+                assert False
+
+    signal_strength = lambda c: c * sum(args[:c])
+    return sum(signal_strength(c) for c in [20, 60, 100, 140, 180, 220])
+
+
+def p2(f):
+    pass
