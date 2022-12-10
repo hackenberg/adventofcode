a2d249c19551fb3a39fd20e1b2358428a917f933
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2022-12-10 20:42:39 +0100

    Day 9: Part 2

--- 2022/day09.py
+++ 2022/day09.py
@@ -1,59 +1,81 @@
 import math
-from collections import deque
 #import IPython; IPython.embed(colors="neutral")
 #import ipdb; ipdb.set_trace()
 
 
-class Grid:
-    def __init__(self):
-        self.head = (0,0)
-        self.tail = (0,0)
-        self.visited = set()
+class Point(tuple):
+    def __add__(self, other):
+        return Point(a + b for a, b in zip(self, other))
 
-    def move(self, direction):
-        assert direction in ["U", "D", "L", "R"]
-        match direction:
-            case "U":
-                self.head = add(self.head, (0,1))
-                if self.distance >= 2:
-                    self.tail = add(self.head, (0,-1))
-            case "D":
-                self.head = add(self.head, (0, -1))
-                if self.distance >= 2:
-                    self.tail = add(self.head, (0,1))
-            case "L":
-                self.head = add(self.head, (-1, 0))
-                if self.distance >= 2:
-                    self.tail = add(self.head, (1,0))
-            case "R":
-                self.head = add(self.head, (1, 0))
-                if self.distance >= 2:
-                    self.tail = add(self.head, (-1,0))
-
-        self.visited.add(self.tail)
-
-    @property
-    def distance(self):
-        dx = abs(self.head[0] - self.tail[0])
-        dy = abs(self.head[1] - self.tail[1])
-        return math.sqrt(dx**2 + dy**2)
+    def __sub__(self, other):
+        return Point(a - b for a, b in zip(self, other))
 
 
-def add(tup1, tup2):
-    return tuple(map(sum, zip(tup1, tup2)))
+DIRS = {
+    "U": Point((0, 1)),
+    "D": Point((0, -1)),
+    "L": Point((-1, 0)),
+    "R": Point((1, 0)),
+}
+
+
+def move(head, tail, direction):
+    if distance(head, tail) < 2:
+        return tail
+
+    match direction:
+        case "U":
+            return head + DIRS["D"]
+        case "D":
+            return head + DIRS["U"]
+        case "L":
+            return head + DIRS["R"]
+        case "R":
+            return head + DIRS["L"]
+
+
+def distance(head, tail):
+    dx, dy = map(abs, head - tail)
+    return math.sqrt(dx**2 + dy**2)
 
 
 def p1(f):
     lines = f.read().splitlines()
-    grid = Grid()
+    head, tail = Point((0, 0)), Point((0, 0))
+    visited = set()
 
     for line in lines:
         direction, steps = line.split()
         for i in range(int(steps)):
-            grid.move(direction)
+            head += DIRS[direction]
+            tail = move(head, tail, direction)
+            visited.add(tail)
 
-    return len(grid.visited)
+    return len(visited)
+
+
+def move2(head, tail):
+    corr = Point(cutoff(x) for x in head - tail)
+    if corr == (head - tail):
+        return tail
+    return tail + corr
+
+
+def cutoff(x):
+    return min(1, max(-1, x))
 
 
 def p2(f):
-    pass
+    lines = f.read().splitlines()
+    rope = [Point((0, 0))] * 10
+    visited = set()
+
+    for line in lines:
+        direction, steps = line.split()
+        for i in range(int(steps)):
+            rope[0] += DIRS[direction]
+            for i in range(1, len(rope)):
+                rope[i] = move2(rope[i-1], rope[i])
+            visited.add(rope[-1])
+
+    return len(visited)
