26897b70c0b8949e1379c521acd87e8f9af9b5c6
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2023-11-03 02:33:11 +0100

    2021: Day 2

---
+++ 2021/day02.py
@@ -0,0 +1,31 @@
+def p1(f):
+    lines = f.read().splitlines()
+    xpos, zpos = 0, 0
+
+    for line in lines:
+        match line.split():
+            case ["forward", n]:
+                xpos += int(n)
+            case ["down", n]:
+                zpos += int(n)
+            case ["up", n]:
+                zpos -= int(n)
+
+    return xpos * zpos
+
+
+def p2(f):
+    lines = f.read().splitlines()
+    xpos, zpos, aim = 0, 0, 0
+
+    for line in lines:
+        match line.split():
+            case ["forward", n]:
+                xpos += int(n)
+                zpos += int(n) * aim
+            case ["down", n]:
+                aim += int(n)
+            case ["up", n]:
+                aim -= int(n)
+
+    return xpos * zpos
